1778d221147d71a7689f2a3bf299f5206667c409
Bruny <brunys@brunysixl.xyz>
2024-10-20 18:34:20 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    prefix: '/static/jade/',
+    bare:'https://learnlotsofmathandhistoryandmore-uk.global.ssl.fastly.net/bear/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
