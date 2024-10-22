391acad1b2a897649740c6a5e4a233d019c8ffb5
Xyphora Network <brunys@brunysixl.xyz>
2024-10-21 22:52:24 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/jade/',
-    bare:'https://learnlotsofmathandhistoryandmore-uk.global.ssl.fastly.net/bear/',
+    bare:'https://bareserver.ubg.icu/bear/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
